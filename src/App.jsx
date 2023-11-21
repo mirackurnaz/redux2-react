164a16f5f3f51bb827fcfa7724d999dcc7bc23a9
Miraç Kurnaz <surrmalatt@gmail.com>
2023-11-21 11:36:36 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 import './App.css'
 import CourseForm from './Components/CourseForm'
 import CourseList from './Components/CourseList'
-
+import CoursePrice from './Components/CoursePrice'
 import CourseSearch from './Components/CourseSearch'
 
 function App() {
@@ -12,8 +12,8 @@ function App() {
     <>
       <CourseForm/>
       <CourseSearch/>
-
       <CourseList/>
+      <CoursePrice/>
    
        
     </>
